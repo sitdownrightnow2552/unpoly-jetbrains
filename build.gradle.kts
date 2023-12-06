b494e65082a73e162627250a6dc0b166ddbbaa17
Toannd <toannd@eway.vn>
2023-12-06 14:15:28 +0700

    build: add token from properties for publishPlugin task

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ tasks {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
@@ -122,7 +122,7 @@ tasks {
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token = environment("PUBLISH_TOKEN")
+        token = properties("idea_plugin_deployment_key").orElse(environment("PUBLISH_TOKEN"))
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
